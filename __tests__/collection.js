6c11674000a6e99084c7cf454f4afac64d8a51da
Arjun Komath <arjunkomath@gmail.com>
2017-02-07 22:54:09 +0530

    Add test

--- __tests__/collection.js
+++ __tests__/collection.js
@@ -1,12 +1,12 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import Collection from '../app/components/collection';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+    const tree = renderer.create(
+        <Collection />
+    );
 });
