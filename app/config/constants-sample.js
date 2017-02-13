b98e02006688a729bfb299edaf0da4482ed3c13f
Arjun Komath <arjunkomath@gmail.com>
2017-02-13 23:52:13 +0530

    Constants file

---
+++ app/config/constants-sample.js
@@ -0,0 +1,13 @@
+/**
+ * Rename this file to constants.js
+ * Replace the dummy values with your actual values
+ */
+export default {
+    client_ids: {
+        ios: 'YOUR_WHITELABEL_IOS_KEY',
+        android: 'YOUR_WHITELABEL_ANDROID_KEY',
+        sound_cloud: 'SOUND_CLOUD_CLIENT_ID'
+    },
+    api_base_url: 'https://beta.whitelabel.cool/api',
+    version: 'v1'
+}
